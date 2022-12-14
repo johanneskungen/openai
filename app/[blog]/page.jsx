4fe59f659422546263887e01a23411077e901bf7
Johannes Eriksson <johannes.einar.eriksson@gmail.com>
2022-12-14 18:24:02 +0100

    seven

--- app/[blog]/page.jsx
+++ app/[blog]/page.jsx
@@ -16,7 +16,7 @@ async function page({params: {blog}}) {
     <div className='mt-24'>
         <h1 className='font-bold text-2xl m-4 text-center'>{singleBlog.title}</h1>
         <p className='w-[85vw] lg:w-[60vw] xl:w-[45vw] mx-auto mb-8'>{singleBlog.body}</p>
-        <Link href="/"><div className='cursor-pointer hover:scale-105 duration-150 hover:rotate-1 bg-[#ff3471] w-[85vw] mx-auto text-white font-semibold px-4 py-2'>previous page</div></Link>
+        <Link href="/"><div className='cursor-pointer hover:scale-105 duration-150 hover:rotate-1 bg-[#ff3471] w-[85vw] mx-auto text-white font-semibold px-4 py-2 lg:w-[60vw] xl:w-[45vw]'>previous page</div></Link>
     </div>
   )
 }
